8301511e16e63fa7a48f62bb80f728edb2217f68
Bo Yao <by677@nyu.edu>
2023-05-18 18:03:24 +0800

    working viewer

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getMessageIds(group) {
+  let res = await fetch("https://j96g3uepe0.execute-api.us-east-1.amazonaws.com/groups/" + group);
+  let {messageIds} = await res.json();
+  return messageIds;
+}
+
+async function renderMessage(group, id) {
+  const script = document.createElement('script');
+  script.src = 'https://telegram.org/js/telegram-widget.js?22';
+  script.async = true;
+  script.setAttribute('data-telegram-post', group + '/' + id);
+  document.body.appendChild(script);
+}
+
+async function renderMessages() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const group = urlParams.get('group') || 'cryptonear';
+  let messageIds = await getMessageIds(group);
+  for (let id of messageIds) {
+    await renderMessage(group, id);
+  }
+  document.getElementById("loading").remove();
+}
+
+renderMessages();
