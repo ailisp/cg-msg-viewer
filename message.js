ed72d0b01ea65c46112ec67c5d1be55d3ef49f20
Bo Yao <by677@nyu.edu>
2023-05-23 10:49:22 +0800

    clean up

--- message.js
+++ message.js
@@ -6,33 +6,11 @@ async function renderMessage(group, id) {
     document.body.appendChild(script);
 }
 
-// async function addIframeResizer() {
-//     const script = document.createElement('script');
-//     script.src = "https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.6/iframeResizer.contentWindow.js";
-//     script.async = true;
-//     document.body.appendChild(script);
-// }
-
-// async function main() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const group = urlParams.get('group') || 'cryptonear';
-    let id = urlParams.get('id');
-    renderMessage(group, id).then(()=>{
+const urlParams = new URLSearchParams(window.location.search);
+const group = urlParams.get('group') || 'cryptonear';
+let id = urlParams.get('id');
+renderMessage(group, id).then(() => {
     document.getElementById("loading").remove();
-    // await addIframeResizer();
-    // if ('parentIFrame' in window) {
-    //     console.log('aaa', document.body.scrollHeight);
-    //     parentIFrame.size(document.body.scrollHeight); // Set height to 100px
-    // } else {
-    //     console.log('no')
-    // }
 })
-// }
-
-// main();
-
-// const iframe = document.createElement('iframe');
-// iframe.src = 'https://t.me/'+group+'/'+id+'?embed=1&userpic=true';
-// document.body.appendChild(iframe);
 
 
